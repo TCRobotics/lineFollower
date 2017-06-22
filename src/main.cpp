fecc76831e1ab068508ec14399cdbda0408a4eeb
Alejandro Taracido Cano <ataracido@elinsa.org>
2017-06-22 13:02:50 +0200

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@
 #include "oled.h"
 #include "elapsedMillis.h"
 #include "IntervalTimer.h"
-//
-
 
 unsigned long sysTickCounts = 0;
 elapsedMicros systickMicros;
@@ -19,14 +17,13 @@ IntervalTimer sysTimer;
 unsigned int sysTickMilisPeriod = 10;
 unsigned int sysTickSecond = 1000/sysTickMilisPeriod;
 
-int wallsDetected = 0;
 bool activateController = false;
 
 void sysTick() {
 DEBUG1ON;
 //DEBUG1OFF;
   systickMicros =0;
-  wallsDetected = readSensors();
+  readSensors();
   updateEncoderData();
   if(activateController)
   {
@@ -53,14 +50,13 @@ void setup() {
   setupPWM();
   setupOLED();
   checkBattery();
-  showData();
+  oledShowData();
   setupInterrupts();
   //tonePlay();
   //while ((readSensors() & 0b010) == 0){}
   delay(1000);
   sysTimer.begin(sysTick, sysTickMilisPeriod*100);
 }
-
 
 
 void loop() {
@@ -72,162 +68,11 @@ void loop() {
   if(sysTickCounts > 4*sysTickSecond)   {RSpeedSet = 70; LSpeedSet = 70;}
   //if(sysTickCounts > (4*sysTickSecond)) {sysTimer.end();}
   */
-/*
-  //activateController = true;
-  //Serial.println(0);
-  //frontMove(CELL / 2);
-  //activateController = false;
-  //motorBrake(RLMOTOR);
-  while(1);
-  Serial.println(1);
-  turna(90);
-  Serial.println(2);
-  frontMove(CELL / 2);
-  Serial.println(3);
-  turna(90);
-  Serial.println(4);
-  frontMove(CELL / 2);
-  Serial.println(5);
-  turna(90);
-  Serial.println(6);
-  frontMove(CELL / 2);
-  Serial.println(7);
-  turna(90);
-  Serial.println(8);
-
-  frontMove(CELL / 2);
-  Serial.println(9);
-  turna(-90);
-  Serial.println(10);
-  frontMove(CELL / 2);
-  Serial.println(11);
-  turna(-90);
-  Serial.println(12);
-  frontMove(CELL / 2);
-  Serial.println(13);
-  turna(-90);
-  Serial.println(14);
-  frontMove(CELL / 2);
-  Serial.println(15);
-  turna(-90);
-  Serial.println(16);
-
-  frontMove(CELL / 2);
-  Serial.println(17);
-  turna(180);
-  Serial.println(18);
-  frontMove(CELL / 2);
-  Serial.println(19);
-  turna(180);
-  Serial.println(20);
-  frontMove(CELL / 2);
-  Serial.println(21);
-  turna(-180);
-  Serial.println(22);
-  frontMove(CELL / 2);
-  Serial.println(23);
-  turna(-180);
-  Serial.println(24);
-
-  while(1)
-  {
-    pivotTurn(360);
-    Serial.println(25);
-    pivotTurn(-360);
-    Serial.println(26);
-  }
-
-  DEBUG2ON;
-  activateController = true;
-  frontMove(STARTMIDLECELL);
-  activateController = false;
-  motorBrake(RLMOTOR);
-  //serialDebug();
-  DEBUG2OFF;
-  while ((readSensors() & 0b010) == 0){}
-
-  activateController = true;
-  pivotTurn(90);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  while ((readSensors() & 0b010) == 0){}
-
-  activateController = true;
-  pivotTurn(-90);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  while ((readSensors() & 0b010) == 0){}
-*/
-
-/*
-  activateController = true;
-  frontMove(STARTMIDLECELL);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  frontMove(2*CELL);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  pivotTurn(90);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  frontMove(CELL);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  pivotTurn(90);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  frontMove(CELL);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  pivotTurn(90);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  frontMove(CELL);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  pivotTurn(90);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  frontMove(-CELL);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  activateController = true;
-  frontMove(-STARTMIDLECELL);
-  activateController = false;
-  motorBrake(RLMOTOR);
-
-  while ((readSensors() & 0b010) == 0){}
-*/
-
-
-
-
   if(sysTickCounts>= 100)
   {
     //serialDebug();
     checkBattery();
-    showData();
+    oledShowData();
     //serialDebug();
     sysTickCounts = 0;
   }
