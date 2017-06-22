2fba1f1c0231e82efe3bc1b2c067649d622b8ac9
Alejandro Taracido Cano <ataracido@elinsa.org>
2017-06-22 13:07:44 +0200

    clean and pin mode config

--- src/pinout.cpp
+++ src/pinout.cpp
@@ -20,23 +20,23 @@ int motorR1Pin          =23;
 int motorR2Pin          =22;
 int motorL1Pin          =21;
 int motorL2Pin          =20;
-unsigned char sensorBar4Pin       =19;
-unsigned char sensorBar3Pin       =18;
-unsigned char sensorBar2Pin       =17;
-unsigned char sensorBar1Pin       =16;
+int sensorBar4Pin       =19;
+int sensorBar3Pin       =18;
+int sensorBar2Pin       =17;
+int sensorBar1Pin       =16;
 int oledResetPin        =15;
 int battSensPin         =14;
 int oledSCKPin          =13;
 
-unsigned char sensorBar6Pin       =28;
-unsigned char sensorBar8Pin       =27;
+int sensorBar6Pin       =28;
+int sensorBar8Pin       =27;
 int sideSensorLeftPin   =26;
 int encoderL2Pin        =25;
 int usrSW2Pin           =24;
 //int frontSensorLeftPin  =A12;
 
-unsigned char sensorBar5Pin       =29;
-unsigned char sensorBar7Pin       =30;
+int sensorBar5Pin       =29;
+int sensorBar7Pin       =30;
 int sideSensorRightPin  =31;
 int encoderL1Pin        =32;
 int usrSW1Pin           =33;
@@ -46,15 +46,22 @@ int usrSW1Pin           =33;
 
 ///////////////////////////////////////
 
-
 void setupPins(){
-  pinMode(irReceivePin,INPUT);
-  pinMode(encoderR1Pin,INPUT);
-  pinMode(encoderR2Pin,INPUT);
-  //pinMode(oledDCPin,OUTPUT);
-  pinMode(buzzerPin,OUTPUT);
-  pinMode(debugPin,OUTPUT);
+  pinMode(encoderR1Pin       ,INPUT);
+  pinMode(encoderR2Pin       ,INPUT);
+  pinMode(encoderL1Pin       ,INPUT);
+  pinMode(encoderL2Pin       ,INPUT);
+  pinMode(motorDriverFaultPin,INPUT);
+  pinMode(usrSW1Pin          ,INPUT);
+  pinMode(usrSW2Pin          ,INPUT);
+  pinMode(irReceivePin       ,INPUT);
 
-
-  //digitalWrite(ledPin, HIGH);
+  pinMode(motorR1Pin         ,OUTPUT);
+  pinMode(motorR2Pin         ,OUTPUT);
+  pinMode(motorL1Pin         ,OUTPUT);
+  pinMode(motorL2Pin         ,OUTPUT);
+  pinMode(edfPWMPin          ,OUTPUT);
+  pinMode(sensBarLedOnPin    ,OUTPUT);
+  pinMode(buzzerPin          ,OUTPUT);
+  pinMode(debugPin           ,OUTPUT);
 }
